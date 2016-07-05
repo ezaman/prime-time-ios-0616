569e3f1e3479dcf8daf8dc6730ec7674d07d1886
ezaman <ehsanmzaman@gmail.com>
2016-07-05 09:54:02 -0400

    generating prime numbers in a tableview

---
+++ PrimeTime/FISPrimeTimeTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FISPrimeTimeTableViewController.h
+//  PrimeTime
+//
+//  Created by Ehsan Zaman on 7/4/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISPrimeTimeTableViewController : UITableViewController
+
+- (NSUInteger)primeNumber:(NSUInteger)prime;
+
+@end
---
+++ PrimeTime/FISPrimeTimeTableViewController.m
@@ -0,0 +1,141 @@
+//
+//  FISPrimeTimeTableViewController.m
+//  PrimeTime
+//
+//  Created by Ehsan Zaman on 7/4/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import "FISPrimeTimeTableViewController.h"
+
+@interface FISPrimeTimeTableViewController ()
+
+@end
+
+@implementation FISPrimeTimeTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //self.tableView.accessibilityIdentifier = @"table";
+    
+    
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+//
+//    return 0;
+//}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return 100;
+}
+
+
+- (NSUInteger)primeNumber:(NSUInteger)prime {
+   
+   NSMutableArray *primeNumbers = [NSMutableArray new];
+    
+    NSInteger number = 0;
+    
+    while (primeNumbers.count <= prime) {
+        
+        number++;
+        
+        BOOL primeNumber = YES;
+        
+        for ( NSInteger i = 2; i < number; i++) {
+            
+            if (number % i == 0) {
+                
+                primeNumber = FALSE;
+                
+            }
+            
+        }
+        
+        if (primeNumber) {
+            
+            [primeNumbers addObject: @(number)];
+            
+        }
+        
+        } NSInteger primeNumber = [primeNumbers[prime] integerValue];
+        
+     return primeNumber;
+}
+
+
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"table" forIndexPath:indexPath];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%li", [self primeNumber:indexPath.row + 1]];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
